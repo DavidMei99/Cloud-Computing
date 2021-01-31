9a9efe2c73388189c0d8d287f774ec8c1a7936b3
htlifelover <htlifelover@gmail.com>
2021-01-31 00:32:26 -0500

    successful testing

--- Project1/src/main/java/edu/cooper/ece465/prim.java
+++ Project1/src/main/java/edu/cooper/ece465/prim.java
@@ -1,7 +1,15 @@
 package edu.cooper.ece465;
 
 import java.util.*;
+import java.text.*;
 
+/**
+* This project uses mulit-threading to implement the Prim's Algorithm,
+*  which aims to find the minimumspanning tree (MST)given an undirected graph.
+*
+* @author Zhihao Wang & Di Mei
+*@ @since 2021-01-29
+*/
 public class prim {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -16,11 +24,27 @@ public class prim {
         System.out.print("\nEnter div number: ");
         div = sc.nextInt();
 
-        int wtMat[][] = {{0,1,3,999,999,2}, {1,0,5,1,999,999}, {3,5,0,2,1,999}, {999,1,2,0,4,999}, {999,999,1,4,0,5}, {2,999,999,999,5,0}};
+        /*int wtMat[][] = {{0,1,3,999,999,2}, {5,0,999,8,999, 6, 999}, {1,999,0,3,2,999, 999},
+                         {4,1,2,0,4,999}, {999,999,1,4,0,5}, {2,999,999,999,5,0}}; */
+//        int wtMat[][] = {{0, 5,1,4,999,999,999}, {5,0,999, 8,999,6, 999}, {1,999,0,3,2,999, 999},
+//                {4,8,3,0,8,999,999}, {999,999,2,999,0,7,9}, {999,999,999,8,7,0,999}, {999,999,999,999,9,999,0}};
+        int wtMat[][] = new int[v][v];
+        Random r=new Random();
+        for(int i=0;i<v;i++) {
+            for (int j = 0; j < v; j++) {
+                wtMat[i][j] = r.nextInt(20);
+            }
+        }
 
         MST mst = new MST();
+        long start = System.currentTimeMillis();
         mst.findMST(wtMat, v, src, div);
-
-
+        long end = System.currentTimeMillis();
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.print("Execution time for div=1 is " + formatter.format((end - start) / 1000d) + " seconds");
+        long start2 = System.currentTimeMillis();
+        mst.findMST(wtMat, v, src, div+3);
+        long end2 = System.currentTimeMillis();
+        System.out.print("Execution time for div=4 is " + formatter.format((end2 - start2) / 1000d) + " seconds");
     }
 }
