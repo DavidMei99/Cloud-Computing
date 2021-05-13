22fd6d377cb34fbfbea3088d7a57ebc185e90144
htlifelover <htlifelover@gmail.com>
2021-05-13 01:36:44 -0400

    security

--- Final_Project/upload-app/src/App.js
+++ Final_Project/upload-app/src/App.js
@@ -9,20 +9,13 @@ import { uploadFile } from 'react-s3';
 import UploadImageToS3WithNativeSdk from "./components/UploadImageToS3WithNativeSdk"
 import UploadImageToS3WithReactS3 from "./components/UploadImageToS3WithReactS3"
 
-const S3_BUCKET ='knnbucket';
-const DIR_NAME = "uploadfiles";
-const REGION ='us-east-1';
-const ACCESS_KEY ='AKIAX6M7U3EZPHC6HEMI';
-const SECRET_ACCESS_KEY ='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';
-
-
 
 const config = {
-    bucketName: S3_BUCKET,
-    dirName: DIR_NAME,
-    region: REGION,
-    accessKeyId: ACCESS_KEY,
-    secretAccessKey: SECRET_ACCESS_KEY,
+    bucketName: process.env.S3_BUCKET,
+    dirName: process.env.DIR_NAME,
+    region: process.env.REGION,
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
 }
 
 // class App extends Component {
